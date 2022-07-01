dc63650b4dff3c96a0a58cfa01425a10d9d96d09
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-02 00:21:54 +0530

    feat: Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="d-flex flex-justify-around align-items-center">
+      <h3>OYODO</h3>
+      <ul className="list-style-none d-flex">
+        <li className="ml-1">HOME</li>
+        <li className="ml-1">DAPIBUS</li>
+        <li className="ml-1">ULTRICIES</li>
+        <li className="ml-1">BIBENDUM</li>
+        <li className="ml-1">MAGNA</li>
+      </ul>
+    </nav>
+  );
 }
