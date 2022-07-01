d312bce72f0f40bacee182c64b5afd81188bf472
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-02 00:53:55 +0530

    feat: fixed navbar to top

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="d-flex flex-justify-around align-items-center p-1 black-bg ">
+    <nav className="d-flex flex-justify-around align-items-center p-1 black-bg navbar">
       <h4 className="aqua-text-dark">OYODO</h4>
       <ul className="list-style-none d-flex">
         <li className="ml-3  grey-text-color font-weight-bold">HOME</li>
