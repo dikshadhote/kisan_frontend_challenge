7aadf9798e08e38c1ed677e15a1695937189bb42
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-02 00:44:45 +0530

    feat: text and background color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="d-flex flex-justify-around align-items-center">
-      <h3>OYODO</h3>
+    <nav className="d-flex flex-justify-around align-items-center p-1 black-bg ">
+      <h4 className="aqua-text-dark">OYODO</h4>
       <ul className="list-style-none d-flex">
-        <li className="ml-1">HOME</li>
-        <li className="ml-1">DAPIBUS</li>
-        <li className="ml-1">ULTRICIES</li>
-        <li className="ml-1">BIBENDUM</li>
-        <li className="ml-1">MAGNA</li>
+        <li className="ml-3  grey-text-color font-weight-bold">HOME</li>
+        <li className="ml-3  grey-text-color font-weight-bold">DAPIBUS</li>
+        <li className="ml-3  grey-text-color font-weight-bold">ULTRICIES</li>
+        <li className="ml-3  grey-text-color font-weight-bold">BIBENDUM</li>
+        <li className="ml-3  grey-text-color font-weight-bold">MAGNA</li>
       </ul>
     </nav>
   );
