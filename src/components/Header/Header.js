03373d5d48606f5cc16ebeca548cfafd8614834b
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-02 14:16:57 +0530

    feat: make text inline and highlight it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <h2 className="grey-light-bg">
-        Duis mollis, est non commondo luctus,nisi erat portitator ligula
-      </h2>
+    <div className="mt-4 p-4 grey-light-bg">
+      <div className="white-text-color p-1 article-header mt-4 font-weight-bold ">
+        <span className="orange-light-bg  article-head-text p-1">
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula,eget
+          lacinia odio sem nec elit.
+        </span>
+      </div>
     </div>
   );
 }
