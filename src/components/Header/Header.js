a6428cd657e84876f6869d027b1cda845d8719cc
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-02 15:24:39 +0530

    feat: content container width

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Header() {
   return (
     <div className="mt-4 p-4 grey-light-bg">
-      <div className="white-text-color p-1 article-header mt-4 font-weight-bold ">
-        <span className="orange-light-bg  article-head-text p-1">
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula,eget
-          lacinia odio sem nec elit.
+      <section className="white-text-color p-1 article-header mt-4 font-weight-bold content-container  ">
+        <span className="orange-light-bg  article-head-text p-1 ">
+          Duis mollis, est non commodo luctus, <br /> nisi erat porttitor
+          ligula,eget lacinia odio <br /> sem nec elit.
         </span>
-      </div>
+      </section>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="d-flex flex-justify-around align-items-center p-1 black-bg navbar">
-      <h4 className="aqua-text-dark">OYODO</h4>
-      <ul className="list-style-none d-flex">
-        <li className="ml-3  grey-text-color font-weight-bold">HOME</li>
-        <li className="ml-3  grey-text-color font-weight-bold">DAPIBUS</li>
-        <li className="ml-3  grey-text-color font-weight-bold">ULTRICIES</li>
-        <li className="ml-3  grey-text-color font-weight-bold">BIBENDUM</li>
-        <li className="ml-3  grey-text-color font-weight-bold">MAGNA</li>
-      </ul>
+    <nav className="black-bg navbar">
+      <div className="d-flex flex-justify-space-between align-items-center content-container ">
+        <h4 className="aqua-text-dark">OYODO</h4>
+        <ul className="list-style-none d-flex">
+          <li className="ml-3  grey-text-color font-weight-bold">HOME</li>
+          <li className="ml-3  grey-text-color font-weight-bold">DAPIBUS</li>
+          <li className="ml-3  grey-text-color font-weight-bold">ULTRICIES</li>
+          <li className="ml-3  grey-text-color font-weight-bold">BIBENDUM</li>
+          <li className="ml-3  grey-text-color font-weight-bold">MAGNA</li>
+        </ul>
+      </div>
     </nav>
   );
 }
